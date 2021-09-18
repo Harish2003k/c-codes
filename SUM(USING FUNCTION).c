b6d423a428e1bbed8622e1485c7b77fceaefbe05
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-09-18 16:27:44 +0530

    sum of two no using function call

---
+++ SUM(USING FUNCTION).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void sum()
+{
+    printf("\n: SUM USING FUNCTION CALLING :");
+    int n1,n2,sum=0;
+    printf("\nEnter The Two number :");
+    scanf("%d%d",&n1,&n2);
+    sum=n1+n2;
+    printf("Sum=%d",sum);
+}
+void main()
+{
+    sum();
+    printf("\n WELCOME TO FUNCTION:");
+    sum();
+    sum();
+}
+
