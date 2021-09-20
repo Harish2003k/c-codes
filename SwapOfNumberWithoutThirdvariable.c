f760e15e9001edafae7df9abd0126cb8415aec8e
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-09-20 21:35:22 +0530

    SwapTheNumber

---
+++ SwapOfNumberWithoutThirdvariable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void swap(int c,int d);
+int main()
+{
+    int a=10,b=20;
+    swap(a,b);
+}
+void swap(int c,int d)
+{
+printf("YOUR FIRST NUMBER IS %d and SECOND NUMBER IS %d",d,c);
+
+}
